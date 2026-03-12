e1f0a54f305c87414aa1e1fb4d812db236af3444
JuanaVillalba <juananicolvillalba@gmail.com>
2026-03-12 11:33:20 -0300

    Terinado

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
-//import "../style/index.css";
+import { name } from "file-loader";
+import "../style/index.css";
 
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
@@ -33,9 +34,10 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1 >${variables.name || "Lucy"} ${variables.lastName ||
+    "Boilett"}</h1>
+          <h2>${variables.role || "Web Developer"}</h2>
+          <h3>${variables.city || "Miami"}, ${variables.country || "USA"}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
@@ -89,17 +91,3 @@ window.onload = function() {
     });
   });
 };
-
-let nombre = document.querySelector("#nombre");
-let apellido = document.getElementById("apellido");
-let rol = document.querySelector(".rol");
-let ubicacion = document.querySelector(".ubicacion");
-let nombreInput = document.querySelector(".nombreInput");
-let apellidoInput= document.querySelector(".apellidoInput");;
-nombreInput.addEventListener("click",()=>{
-  nombre.innerText = nombreInput.value;
-})
-apellidoInput.addEventListener("click",()=>{
-  apellido.innerText = apellidoInput.value;
-})
-console.log(nombreInput);
